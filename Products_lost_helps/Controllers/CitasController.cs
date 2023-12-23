70b9d735b5f4ad146a36274e6ea445be974c4c09
Wilson Javier Estrada Zapata <wilson.estrada@zibordesarrolladores.com>
2023-12-22 19:32:40 -0500

    cambio de background

---
+++ Products_lost_helps/Controllers/CitasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Products_lost_helps.Controllers
+{
+    public class CitasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Products_lost_helps/Controllers/PrincipalController.cs
+++ Products_lost_helps/Controllers/PrincipalController.cs
@@ -20,7 +20,26 @@ namespace Products_lost_helps.Controllers
             return View(productos);
         }
 
+        public IActionResult Servicios()
+        {
+            List<Products> productos = _loUsuario.GetAllProducts();
+            return View(productos);
+        }
+
         public ActionResult ver(int IdProducts)
+        {
+            //Lo_Usuario lo = new Lo_Usuario();
+            DataTable dt = _loUsuario.GetImagenes();
+            DataRow row = dt.AsEnumerable().FirstOrDefault(r => r.Field<int>("IdProducto") == IdProducts);
+            if (row != null)
+            {
+                byte[] imgc = (byte[])row["Imagen"];
+                return File(imgc, "application/octet-stream");
+            }
+            return RedirectToAction("Index", "Principal");
+        }
+
+        public ActionResult verfirst(int IdProducts)
         {
             //Lo_Usuario lo = new Lo_Usuario();
             DataTable dt = _loUsuario.GetImagenes();
